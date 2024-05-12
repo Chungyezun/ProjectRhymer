3da9911566a550f4d5de772693c7a1f75e352680
Chungyezun <maple0729@kaist.ac.kr>
2024-05-12 16:30:43 +0900

    add semantic similarity check function

--- semantics.py
+++ semantics.py
@@ -3,9 +3,7 @@ from nltk.corpus import wordnet as wn
 
 brown_ic = wn.ic(nltk.corpus.brown, False, 0.0)
 
-# I have forgotten that tomato is rotten.
-
-words = [('I', ["I"]),('have', ['have']),('forgotten', ['rotten', "got", "garden"]), ('.', ['.'])]
+# words = [('I', ["I"]),('have', ['have']),('forgotten', ['rotten', "got", "garden"]), ('.', ['.'])]
 
 def get_wordnet_pos(treebank_tag):
     if treebank_tag.startswith('J'):
@@ -19,25 +17,26 @@ def get_wordnet_pos(treebank_tag):
     else:
         return None
 
-def recommend_semantics_wordnet(mapped_words):
+def are_meanings_similar(word1, word2, threshold):
+    synsets1 = wn.synsets(word1)
+    synsets2 = wn.synsets(word2)
 
-    output = ""
-    original_sentence = nltk.pos_tag([wp[0] for wp in mapped_words])
-    for wp1 in mapped_words:
-        for word_similar_pron in wp1[1]:
-            for syn_sim in wn.synsets(word_similar_pron):
-                for w in original_sentence:
-                    for syn_original in wn.synsets(w[0]):
-                        if syn_original.pos() == get_wordnet_pos(w[1]) and syn_original.pos() == syn_sim.pos():
-                            similarity = syn_sim.res_similarity(syn_original, brown_ic)
-                            if similarity > 3:
-                                print("original word: ", syn_original, "similar word: ", syn_sim, "similarity: ", similarity)
-                                print("sim: ", wp1[1])
-                                print("original: ", w[0])
+    max_similarity = 0.0
 
-# recommend_semantics_wordnet(words)
-a = wn.synsets("i")
-s1 = wn.synset('dog.n.01').pos()
-print(a)
-# s2 = wn.synset("pencil.n.01")
-# print(s1.res_similarity(s2,brown_ic))
+    for synset1 in synsets1:
+        for synset2 in synsets2:
+            # similarity = synset_sim.res_similarity(synset_original, brown_ic)
+            similarity = synset1.wup_similarity(synset2)
+            if similarity and similarity > max_similarity:
+                max_similarity = similarity
+
+    return max_similarity > threshold
+
+def recommend_semantics_wordnet(tokens : list[(str, list[str])], threshold = 0.8):
+    # output = ""
+    original_sentence = nltk.pos_tag([t[0] for t in tokens])
+    for token in tokens:
+        for word_similar_pron in token[1]:
+            for w in original_sentence:
+                if token[0] != w[0] and are_meanings_similar(word_similar_pron, w[0], threshold):
+                    print("original word: ", token[0], " , ", "replaced word: ", w[0], " , ", "similar word: ", word_similar_pron)
